c621ed1bf68ba99d64deb6c30d400dae4104f3e6
bartekus <bartekus@gmail.com>
2021-01-03 23:21:31 -0700

    Adjust proxy setting for client

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+
 import { Message } from '@forte-dev/api-interfaces';
 
 const app = express();
@@ -9,8 +10,11 @@ app.get('/api', (req, res) => {
   res.send(greeting);
 });
 
-const port = process.env.port || 3333;
+const port = Number(process.env.SERVER_PORT) || 4444;
+const hostname = process.env.HOSTNAME || 'localhost';
+
 const server = app.listen(port, () => {
-  console.log('Listening at http://localhost:' + port + '/api');
+  console.log(`Listening at http://${hostname}:${port}/api`);
 });
+
 server.on('error', console.error);
