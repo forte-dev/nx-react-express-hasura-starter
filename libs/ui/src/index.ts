369161dde37561266807d36270572b7fdc81c403
bartekus <bartekus@gmail.com>
2021-01-11 21:20:05 -0700

    Refactor ui lib entry file

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,3 +1,12 @@
-export * from './lib/utils/index';
-export * from './lib/theme/index';
-export * from './lib/layouts/index';
+export * from './components/homepage-main/homepage-main';
+export * from './components/footer/footer';
+export * from './components/homepage-hero/homepage-hero';
+export * from './components/top-nav-bar/top-nav-bar';
+export * from './components/copyright/copyright';
+export * from './components/theme-palette-mode-toggle-button/theme-palette-mode-toggle-button';
+
+export { default as AppLayout, PaletteModeContext } from './layouts/app-layout/app-layout';
+
+export * from './utils/get-cookie/get-cookie';
+
+export { default as ScrollToTop } from './utils/scroll-to-top/scroll-to-top';
