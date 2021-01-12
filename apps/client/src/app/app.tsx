0e41b67a546208466838a83bd245928361138e8e
bartekus <bartekus@gmail.com>
2021-01-11 21:23:39 -0700

    Refactor client to use mui with custom theme & light/dark mode

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,24 +1,13 @@
-import React, { useEffect, useState } from 'react';
-
-import { Message } from '@forte-dev/api-interfaces';
-import { AppLayout } from '@forte-dev/app-layout';
+import React from 'react';
+import { AppLayout, TopNavBar, HomepageMain, HomepageHero, Footer } from '@forte-dev/ui';
 
 export const App = () => {
-  const [m, setMessage] = useState<Message>({ message: '' });
-
-  useEffect(() => {
-    fetch('/api')
-      .then((r) => r.json())
-      .then(setMessage);
-  }, []);
-
   return (
     <AppLayout>
-      <div style={{ textAlign: 'center' }}>
-        <h1>Welcome to client!</h1>
-        <img width="450" src="https://raw.githubusercontent.com/nrwl/nx/master/images/nx-logo.png" />
-      </div>
-      <div>{m.message}</div>
+      <TopNavBar />
+      <HomepageHero />
+      <HomepageMain />
+      <Footer />
     </AppLayout>
   );
 };
--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
+import { ScrollToTop } from '@forte-dev/ui';
 import App from './app/app';
-import { ScrollToTop } from '@forte-dev/scroll-to-top';
 
 ReactDOM.render(
   <React.StrictMode>
