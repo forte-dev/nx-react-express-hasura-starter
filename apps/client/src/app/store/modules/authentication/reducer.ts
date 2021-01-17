aee2d33d2ef8f4866116b262fb84c2ee55ee3860
bartekus <bartekus@gmail.com>
2021-01-17 11:48:33 -0700

    Add authentication module to redux

---
+++ apps/client/src/app/store/modules/authentication/reducer.ts
@@ -0,0 +1,66 @@
+import {
+  SIGN_UP_REQUEST,
+  SIGN_UP_SUCCESS,
+  SIGN_UP_ERROR,
+  LOGIN_REQUEST,
+  LOGIN_SUCCESS,
+  LOGIN_ERROR,
+  LOGOUT,
+} from './actions';
+
+const initialState = {
+  signUpPending: false,
+  loginPending: false,
+  authenticated: false,
+};
+
+export default function auth0(state = initialState, action) {
+  switch (action.type) {
+    case SIGN_UP_REQUEST:
+      return {
+        ...initialState,
+        signUpPending: true,
+      };
+
+    case SIGN_UP_SUCCESS:
+      return {
+        ...state,
+        signUpPending: false,
+        ...action.payload,
+      };
+
+    case SIGN_UP_ERROR:
+      return {
+        ...state,
+        signUpPending: false,
+        ...action.error,
+      };
+
+    case LOGIN_REQUEST:
+      return {
+        ...initialState,
+        loginPending: true,
+      };
+
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        loginPending: false,
+        authenticated: true,
+        ...action.payload,
+      };
+
+    case LOGIN_ERROR:
+      return {
+        ...state,
+        loginPending: false,
+        ...action.error,
+      };
+
+    case LOGOUT:
+      return initialState;
+
+    default:
+      return state;
+  }
+}
---
+++ apps/client/src/app/store/modules/authentication/sagas.ts
@@ -0,0 +1,35 @@
+import { put, call, takeLatest } from 'redux-saga/effects';
+
+import {
+  SIGN_UP_REQUEST,
+  signUpSuccess,
+  signUpError,
+  LOGIN_REQUEST,
+  loginSuccess,
+  loginError,
+} from './actions';
+
+export function* signUpSaga(AuthService, action) {
+  try {
+    const response = yield call(AuthService.signUp, action.payload);
+
+    yield put(signUpSuccess(response));
+  } catch (err) {
+    yield put(signUpError(err));
+  }
+}
+
+export function* loginSaga(AuthService, action) {
+  try {
+    const response = yield call(AuthService.login, action.payload);
+
+    yield put(loginSuccess(response));
+  } catch (err) {
+    yield put(loginError(err));
+  }
+}
+
+export default function* ({ AuthService }) {
+  yield takeLatest(SIGN_UP_REQUEST, signUpSaga, AuthService);
+  yield takeLatest(LOGIN_REQUEST, loginSaga, AuthService);
+}
