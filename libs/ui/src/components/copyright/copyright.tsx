8dce135e02bd2da2eb2714252207dd3e7beeba1c
bartekus <bartekus@gmail.com>
2021-01-17 22:44:06 -0700

    Add props spreading to copyright component

--- libs/ui/src/components/copyright/copyright.tsx
+++ libs/ui/src/components/copyright/copyright.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
+import { BoxTypeMap } from '@material-ui/core';
 
 /* eslint-disable-next-line */
 export interface CopyrightProps {}
 
-export function Copyright(props: CopyrightProps) {
+export function Copyright(props: any) {
   return (
-    <Typography variant="body2" color="textSecondary" align="center">
+    <Typography variant="body2" color="textSecondary" align="center" {...props}>
       {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
         Your Website
