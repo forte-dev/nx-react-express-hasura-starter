75e8aceabda4efcc6255fc90af4086618e160a45
bartekus <bartekus@gmail.com>
2021-01-17 22:47:52 -0700

    Cleanup landing-page

---
+++ apps/client/src/app/pages/landing-page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { HomepageHero, HomepageMain, LoginRedirect } from '@forte-dev/ui';
+
+export default function LandingPage() {
+  return (
+    <>
+      <HomepageHero />
+      <HomepageMain />
+      <LoginRedirect />
+    </>
+  );
+}
